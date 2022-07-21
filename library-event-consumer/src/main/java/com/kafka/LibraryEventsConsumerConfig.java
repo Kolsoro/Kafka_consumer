67364215777134610b084be72e1d7ccf29d600ea
samergosain <samergosain@localhost>
2022-07-21 18:30:19 +0530

    kafka consumer implemented

---
+++ library-event-consumer/src/main/java/com/kafka/LibraryEventsConsumerConfig.java
@@ -0,0 +1,12 @@
+package com.kafka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+
+	
+	
+}
